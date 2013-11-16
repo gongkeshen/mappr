43d1e43f51fe46cef6bdf4d19c0841fb419ae4d6
Gong Ke Gouldstone <fifo@fifo-VirtualBox.(none)>
2013-11-16 13:30:57 -0500

    Initial commit with no changes.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mappr::Application.config.secret_key_base = '4dd26c06dddde98da80cacc731ef2455750fc0eb5fab91b2cf8dff7b05f1d9bff89fab312357349fad017255daf4b95092b5ce2b38e6bf697b9c746d10e58eb7'
