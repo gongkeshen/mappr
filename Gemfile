0929eab6446198cdf75fe48a19f0f9d35a598a40
Gong Ke Gouldstone <fifo@fifo-VirtualBox.(none)>
2013-11-16 14:53:28 -0500

    Added form, gems for styling

--- Gemfile
+++ Gemfile
@@ -32,6 +32,12 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+gem 'simple_form'
+
+gem 'country_select'
+
+gem 'twitter-bootstrap-rails'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,4 +4,24 @@ class PlacesController < ApplicationController
 		# Place.all would give the tneire database... which may take a while to load
 		@place = Place.last
 	end
+
+	def new
+		# prepopulate
+		# @place = Place.last
+
+		# get a new place
+		@place = Place.new
+	end
+
+	def create
+		# Place.create (:title=>'omg',)
+		@place = Place.create( place_params )
+		redirect_to root_path
+	end
+
+	private
+
+	def place_params
+		params.require(:place).permit(:title, :address)
+	end
 end
